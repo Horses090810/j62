0453c75f71602ac36b9f84f2bfbd11e0d66c2cf6
Horses090810 <Horses090810@168.com>
2019-09-02 09:27:21 +0800

    h2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Hello World!");
         System.out.println("bbb");
         System.out.println("aaaa");
+        System.out.println("kh");
+        System.out.println("nihao");
     }
 }
